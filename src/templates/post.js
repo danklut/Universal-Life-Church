a66eb88b52d86b1064781f8110d9a03252f1a5a3
danklut <aomcloudenterprise@outlook.com>
2018-08-30 18:23:06 -0700

    Update post.js

--- src/templates/post.js
+++ src/templates/post.js
@@ -1,21 +1,25 @@
+
 import React from 'react';
-import PropTypes from 'prop-types';
 import Helmet from 'react-helmet';
-import { Link, graphql } from 'gatsby';
+import { Link } from 'gatsby';
 import styled from 'styled-components';
 import kebabCase from 'lodash/kebabCase';
-import { Layout, Wrapper, Header, Subline, SEO, PrevNext } from 'components';
+import Layout from '../components/Layout';
+import SEO from '../components/SEO';
+import Wrapper from '../components/Wrapper';
+import Header from '../components/Header';
+import Subline from '../components/Subline';
 import { media } from '../utils/media';
+
 import config from '../../config/SiteConfig';
 import '../utils/prismjs-theme.css';
 
 const Content = styled.article`
   grid-column: 2;
   box-shadow: 0 4px 120px rgba(0, 0, 0, 0.1);
-  max-width: 1000px;
   border-radius: 1rem;
   padding: 2rem 4rem;
-  background-color: ${props => props.theme.colors.bg};
+  background-color: ${props => props.theme.bg};
   z-index: 9000;
   margin-top: -3rem;
   @media ${media.tablet} {
@@ -35,7 +39,7 @@ const PostContent = styled.div`
 `;
 
 const Post = props => {
-  const { slug, prev, next } = props.pageContext;
+  const { slug } = props.pageContext;
   const postNode = props.data.markdownRemark;
   const post = postNode.frontmatter;
 
@@ -54,7 +58,6 @@ const Post = props => {
             <Link to={`/categories/${kebabCase(post.category)}`}>{post.category}</Link>
           </Subline>
           <PostContent dangerouslySetInnerHTML={{ __html: postNode.html }} />
-          <PrevNext prev={prev} next={next} />
         </Content>
       </Wrapper>
     </Layout>
@@ -63,24 +66,7 @@ const Post = props => {
 
 export default Post;
 
-Post.propTypes = {
-  pageContext: PropTypes.shape({
-    slug: PropTypes.string.isRequired,
-    next: PropTypes.object,
-    prev: PropTypes.object,
-  }),
-  data: PropTypes.shape({
-    markdownRemark: PropTypes.object.isRequired,
-  }).isRequired,
-};
-
-Post.defaultProps = {
-  pageContext: PropTypes.shape({
-    next: null,
-    prev: null,
-  }),
-};
-
+/* eslint no-undef: off */
 export const postQuery = graphql`
   query postBySlug($slug: String!) {
     markdownRemark(fields: { slug: { eq: $slug } }) {
