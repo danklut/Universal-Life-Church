ab46aebcdce664a24efd310d2072eb3f59cf7f65
danklut <aomcloudenterprise@outlook.com>
2018-08-30 17:43:14 -0700

    Update article.js

--- src/components/article.js
+++ src/components/article.js
@@ -1,10 +1,10 @@
+
 import React from 'react';
-import PropTypes from 'prop-types';
 import styled from 'styled-components';
-import { Link } from 'gatsby';
+import Link from 'gatsby-link';
 import kebabCase from 'lodash/kebabCase';
 
-import { Subline } from 'components';
+import Subline from './Subline';
 
 const Post = styled.article`
   display: flex;
@@ -53,12 +53,3 @@ const Article = ({ title, date, excerpt, slug, timeToRead, category }) => {
 };
 
 export default Article;
-
-Article.propTypes = {
-  title: PropTypes.string.isRequired,
-  date: PropTypes.string.isRequired,
-  excerpt: PropTypes.string.isRequired,
-  slug: PropTypes.string.isRequired,
-  timeToRead: PropTypes.number.isRequired,
-  category: PropTypes.string.isRequired,
-};
