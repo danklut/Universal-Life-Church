6a5d213dde700db1907a79b916059c872543a78a
danklut <aomcloudenterprise@outlook.com>
2018-08-30 19:24:02 -0700

    Create SiteConfig.js

---
+++ config/SiteConfig.js
@@ -0,0 +1,20 @@
+
+module.exports = {
+  pathPrefix: '/', // Prefix for all links. If you deploy your site to example.com/portfolio your pathPrefix should be "portfolio"
+
+  siteTitle: 'Universal Life Church', // Navigation and Site Title
+  siteTitleAlt: 'Minimal Blog - Gatsby Starter', // Alternative Site title for SEO
+  siteUrl: 'https://Universal-Life-Church.netlify.com', // Domain of your site. No trailing slash!
+  siteLanguage: 'en', // Language Tag on <html> element
+  siteBanner: '/social/banner.jpg', // Your image for og:image tag. You can find it in the /static folder
+  favicon: 'src/favicon.png', // Your image for favicons. You can find it in the /src folder
+  siteDescription: 'Minimal Blog with big typography', // Your site description
+
+  // siteFBAppID: '123456789', // Facebook App ID - Optional
+  userTwitter: '@klutmeye', // Twitter Username - Optional
+  ogSiteName: 'danklutmeyer7', // Facebook Site Name - Optional
+
+  // Manifest and Progress color
+  // See: https://developers.google.com/web/fundamentals/web-app-manifest/
+  themeColor: '#3498DB',
+  backgroundColor: '#2b2e3c',
