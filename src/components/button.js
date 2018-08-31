480b5cb2066012c9f172f0e076d8a4be09f51be9
danklut <aomcloudenterprise@outlook.com>
2018-08-30 17:44:26 -0700

    Update button.js

--- src/components/button.js
+++ src/components/button.js
@@ -1,8 +1,10 @@
+
+import React from 'react';
 import styled from 'styled-components';
 import { lighten } from 'polished';
 
-const Button = styled.button`
-  background: ${props => props.theme.colors.primary};
+const Wrapper = styled.button`
+  background: ${props => props.theme.primary};
   border: none;
   display: inline-flex;
   align-items: center;
@@ -10,10 +12,10 @@ const Button = styled.button`
   font-size: ${props => (props.big ? '1.2rem' : '1rem')};
   color: white;
   padding: ${props => (props.big ? '0.35rem 1.6rem' : '0.25rem 1.5rem')};
-  transition: all ${props => props.theme.transitions.normal};
+  transition: all ${props => props.theme.transitionTime};
   box-shadow: 0 4px 6px rgba(50, 50, 93, 0.11), 0 1px 3px rgba(0, 0, 0, 0.08);
   &:hover {
-    background: ${props => lighten(0.05, props.theme.colors.primary)};
+    background: ${props => lighten(0.05, props.theme.primary)};
     cursor: pointer;
     transform: translateY(-2px);
   }
@@ -27,5 +29,7 @@ const Button = styled.button`
     fill: white;
   }
 `;
+
+const Button = props => <Wrapper big={props.big}>{props.children}</Wrapper>;
 
 export default Button;
