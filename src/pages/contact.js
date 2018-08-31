afcff09482c6f6cc9247390056122ec010e4fa89
danklut <aomcloudenterprise@outlook.com>
2018-08-30 18:01:41 -0700

    Update contact.js

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,10 +1,14 @@
+
 /* eslint jsx-a11y/label-has-for:0 */
 
 import React from 'react';
 import Helmet from 'react-helmet';
 import { Link } from 'gatsby';
 import styled from 'styled-components';
-import { Layout, Wrapper, Header, Button } from 'components';
+import Layout from '../components/Layout';
+import Wrapper from '../components/Wrapper';
+import Header from '../components/Header';
+import Button from '../components/Button';
 import { media } from '../utils/media';
 
 import config from '../../config/SiteConfig';
@@ -14,7 +18,7 @@ const Content = styled.div`
   box-shadow: 0 4px 120px rgba(0, 0, 0, 0.1);
   border-radius: 1rem;
   padding: 2rem 4rem;
-  background-color: ${props => props.theme.colors.bg};
+  background-color: ${props => props.theme.bg};
   z-index: 9000;
   margin-top: -3rem;
   @media ${media.tablet} {
@@ -50,8 +54,15 @@ const Contact = () => (
       </Header>
       <Content>
         <h1>Contact</h1>
-        <p>Hello, If you have any questions or would like to donate more to our worthy cause please contact us.Thank you!.</p>
-        <form name="contact-form" method="post" data-netlify="true" data-netlify-honeypot="bot-field" action="/success">
+        <p>If you have any questions or you would like to have your product advertised on our site please contact us. </p>
+        <form
+          name="contact-form"
+          method="post"
+          data-netlify="true"
+          data-netlify-honeypot="bot-field"
+          netlify
+          action="/contact"
+        >
           <p>
             <label htmlFor="name">Name</label>
             <input name="name" type="text" required />
